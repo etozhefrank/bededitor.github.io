332646fbcc9f3d228032fb47b6f7ca76d9fcafaa
etozhefrank <51893554+etozhefrank@users.noreply.github.com>
2020-04-10 18:36:01 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-import * as THREE from 'js/three.module.js';
-import { ARButton } from 'js/ARButton.js'
-
 const LOADER = document.getElementById('js-loader');
 
 const TRAY = document.getElementById('js-tray-slide');
@@ -285,28 +282,6 @@ controls.dampingFactor = 0.1;
 controls.autoRotate = false; // Toggle this if you'd like the chair to automatically rotate
 controls.autoRotateSpeed = 0.2; // 30
 
-//AR
-function onSelect() {
-
-					if ( reticle.visible ) {
-
-						var material = new THREE.MeshPhongMaterial( { color: 0xffffff * Math.random() } );
-						var mesh = new THREE.Mesh( theModel, material );
-						mesh.position.setFromMatrixPosition( reticle.matrix );
-						mesh.scale.y = Math.random() * 2 + 1;
-						scene.add( mesh );
-
-controller = renderer.xr.getController( 0 );
-				controller.addEventListener( 'select', onSelect );
-				scene.add( controller );
-
-				reticle = new THREE.Mesh(
-					new THREE.RingBufferGeometry( 0.15, 0.2, 32 ).rotateX( - Math.PI / 2 ),
-					new THREE.MeshBasicMaterial()
-				);
-				reticle.matrixAutoUpdate = false;
-				reticle.visible = false;
-				scene.add( reticle );
 
 function animate() {
 
@@ -458,62 +433,7 @@ function slide(wrapper, items) {
   items.addEventListener('touchend', dragEnd);
   items.addEventListener('touchmove', dragAction);
 
-  //ARrender
-  function render( timestamp, frame ) {
-
-				if ( frame ) {
-
-					var referenceSpace = renderer.xr.getReferenceSpace();
-					var session = renderer.xr.getSession();
-
-					if ( hitTestSourceRequested === false ) {
-
-						session.requestReferenceSpace( 'viewer' ).then( function ( referenceSpace ) {
-
-							session.requestHitTestSource( { space: referenceSpace } ).then( function ( source ) {
-
-								hitTestSource = source;
-
-							} );
-
-						} );
-
-						session.addEventListener( 'end', function () {
-
-							hitTestSourceRequested = false;
-							hitTestSource = null;
-
-						} );
-
-						hitTestSourceRequested = true;
-
-					}
-
-					if ( hitTestSource ) {
-
-						var hitTestResults = frame.getHitTestResults( hitTestSource );
-
-						if ( hitTestResults.length ) {
-
-							var hit = hitTestResults[ 0 ];
-
-							reticle.visible = true;
-							reticle.matrix.fromArray( hit.getPose( referenceSpace ).transform.matrix );
-
-						} else {
-
-							reticle.visible = false;
-
-						}
-
-					}
-
-				}
-
-				renderer.render( scene, camera );
-
-			}
-
+ 
 
   function dragStart(e) {
     e = e || window.event;
